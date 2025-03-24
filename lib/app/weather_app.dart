f669a2efa07aa144cb7146549fe4e85829dd8f84
Le Dat <tuandat071101@gmail.com>
2025-03-24 08:41:24 +0700

    initial

---
+++ lib/app/weather_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
